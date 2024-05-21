15f454dc2a3fc80cfdaccc5cd5d81e86db89d8df
obmslab <banothe@nitrkl.ac.in>
2024-05-21 17:24:57 +0530

    INIT

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Team from "./components/Team";
+import Publications from "./components/Publications";
+import Research from "./components/Research";
+import Courses from "./components/Courses";
+import LabFacilities from "./components/LabFacilities";
+import Gallery from "./components/Gallery";
+import Services from "./components/Services";
+import Fundings from "./components/Fundings";
+import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/react";
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/Team" element={<Team />} />
+          <Route path="/Publications" element={<Publications />} />
+          <Route path="/Research" element={<Research />} />
+          <Route path="/Courses" element={<Courses />} />
+          <Route path="/LabFacilities" element={<LabFacilities />} />
+          <Route path="/Gallery" element={<Gallery />} />
+          <Route path="/Services" element={<Services />} />
+          <Route path="/Fundings" element={<Fundings />} />
+        </Routes>
+      </div>
+      <Footer />
+      <Analytics />
+    </Router>
+  );
+};
+
+export default App;
